88810e5f04f2f98aeee50dfd530bd01119b400f9
Pepi10 <anabiotic1@gmail.com>
2024-07-10 17:41:25 +0900

    hmm

--- test_code/test.py
+++ test_code/test.py
@@ -1,6 +1,8 @@
 from util.analysis_realtime import analysis
 import cv2
 import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
 
 # Initializing
 cap = cv2.VideoCapture(0)
@@ -9,6 +11,6 @@ ana = analysis()
 
 
 if __name__ == "__main__":
-    frame = cv2.imread('test.jpeg')
+    frame = cv2.imread('test_1.jpeg')
     bm = ana.detect_face(frame)
-    cv2.imwrite('test_result.jpeg', bm)
+    cv2.imwrite('test_1_result.jpeg', bm)
